8b11a524cf9e1d0d84fe99ce4ac150356b681a98
Chris Seale <sealechris@gmail.com>
2017-01-21 22:44:57 -0600

    Working with spread operators

--- bundle.js
+++ bundle.js
@@ -1,3 +1,20 @@
 "use strict";
 
-var x = [1, 2, 3];
+var ourFunction = function ourFunction() {
+    var x = "this is in the outer scope";
+
+    if (true) {
+        var _x = "this is a new value";
+        console.log(_x);
+    }
+
+    console.log(x);
+};
+
+ourFunction();
+
+var awesomeBands = ["Jack White", "Smashing Pumpkins"];
+
+var bands = ["Tool", "Medicine", "Morphine"].concat(awesomeBands);
+
+console.log(bands);
--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const x = [1, 2, 3];
+var ourFunction = function() {
+    let x = "this is in the outer scope";
+
+    if(true) {
+        let x = "this is a new value";
+        console.log(x);
+    }
+
+    console.log(x);
+}
+
+ourFunction();
+
+
+let awesomeBands = ["Jack White", "Smashing Pumpkins"]
+
+let bands = ["Tool", "Medicine", "Morphine", ...awesomeBands];
+
+console.log(bands);
 
