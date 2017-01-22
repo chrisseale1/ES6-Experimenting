affec13ca27c2e29aeb93d4627852c2525cef9c4
Chris Seale <sealechris@gmail.com>
2017-01-21 23:12:26 -0600

    exploring fat arrows

--- bundle.js
+++ bundle.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//CONST, VAR, LET
+
 var ourFunction = function ourFunction() {
     var x = "this is in the outer scope";
 
@@ -13,8 +15,42 @@ var ourFunction = function ourFunction() {
 
 ourFunction();
 
+//SPREAD OPERATORS
 var awesomeBands = ["Jack White", "Smashing Pumpkins"];
 
 var bands = ["Tool", "Medicine", "Morphine"].concat(awesomeBands);
 
 console.log(bands);
+
+//OBJECT SHORTHAND CREATION
+
+var myObject = { x: "cat", y: "dog" };
+
+console.log(myObject.x);
+
+var engine = "Vroom!";
+var wheel = "ssssssshhhhhhh";
+var horn = "Honk!";
+
+var carSounds = { engine: engine, wheel: wheel, horn: horn };
+
+console.log(carSounds);
+
+//Fat Arrows (na na na gonna have a good time)
+var oldway = function oldway() {
+    alert("hello world");
+};
+
+oldway();
+
+var newWay = function newWay() {
+    alert("Hello World. Gonna have a good time!");
+};
+
+newWay();
+
+var newWayWithArg = function newWayWithArg(arg1, arg2) {
+    alert(arg1 + arg2);
+};
+
+newWayWithArg("You're a ", "sly one, Mr. Grinch.");
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//CONST, VAR, LET
+
 var ourFunction = function() {
     let x = "this is in the outer scope";
 
@@ -11,10 +13,46 @@ var ourFunction = function() {
 
 ourFunction();
 
-
+//SPREAD OPERATORS
 let awesomeBands = ["Jack White", "Smashing Pumpkins"]
 
 let bands = ["Tool", "Medicine", "Morphine", ...awesomeBands];
 
 console.log(bands);
 
+//OBJECT SHORTHAND CREATION
+
+let myObject = { x: "cat", y: "dog"};
+
+console.log(myObject.x);
+
+
+const engine = "Vroom!";
+const wheel = "ssssssshhhhhhh";
+const horn = "Honk!";
+
+const carSounds = {engine, wheel, horn};
+
+console.log(carSounds);
+
+//Fat Arrows (na na na gonna have a good time)
+//old standard way
+const oldway = function (){
+    alert("hello world");
+}
+
+oldway();
+
+//new way fat arrows
+const newWay =()=>{
+    alert("Hello World. Gonna have a good time!");
+}
+
+newWay();
+
+//fat arrow with arguments
+const newWayWithArg =(arg1, arg2)=>{
+    alert(arg1 + arg2);
+}
+
+newWayWithArg("You're a ", "sly one, Mr. Grinch.");
